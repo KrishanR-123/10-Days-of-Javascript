13bcd7d083a512e918affa32515afeda2d419ff4
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-09 00:10:42 +0530

    Hackerrank solution

--- Day 0: Hello, World!.js
+++ Day 0: Hello, World!.js
@@ -22,6 +22,7 @@ function readLine() {
     return inputString[currentLine++];
 }
 
+
 /**
 *   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
 *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
