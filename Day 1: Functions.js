571a51495facd4652d396a8bfeed0d5e2808b929
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-09 20:12:29 +0530

    Hackerrank solution

---
+++ Day 1: Functions.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+function factorial(n){
+    let i;
+    let fact = 1;
+    for (i=1; i<=n ; i++) {
+    
+        fact *= i;
+    }
+    return fact;
+}
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
