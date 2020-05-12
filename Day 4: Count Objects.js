87888edc863c09b2baeaf38ec5499d498ede9884
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-12 22:56:08 +0530

    Update Day 4: Count Objects.js

--- Day 4: Count Objects.js
+++ Day 4: Count Objects.js
@@ -21,3 +21,21 @@ process.stdin.on('end', _ => {
 function readLine() {
     return inputString[currentLine++];
 }
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    var count=0;
+    for (var i=0 ; i < objects.length ; i++)
+    {
+       if(objects[i].x == objects[i].y) 
+       {
+        count++;
+      }
+    }
+    return count;
+}
