ab1da4b3d3739bf89cc2b8f5e8ad73c2bd0423bc
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-12 22:56:36 +0530

    Hackerrank solution

--- Day 4: Count Objects.js
+++ Day 4: Count Objects.js
@@ -39,3 +39,16 @@ function getCount(objects) {
     }
     return count;
 }
+
+function main() {
+    const n = +(readLine());
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ');
+        
+        objects.push({x: +(a), y: +(b)});
+    }
+    
+    console.log(getCount(objects));
+}
