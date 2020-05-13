c1725bfc5105aa88a350b465bcfbffccde6208d2
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-13 16:23:50 +0530

    Update Day 5: Template Literals.js

--- Day 5: Template Literals.js
+++ Day 5: Template Literals.js
@@ -21,3 +21,26 @@ process.stdin.on('end', _ => {
 function readLine() {
     return inputString[currentLine++];
 }
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+
+    function sides(literals, ...expressions) {
+    
+    const [a, p] = expressions;
+    
+    const root = Math.sqrt((p*p)-(16*a))
+    const s1 = (p + root)/4;
+    const s2 = (p - root)/4;
+   
+    return ([s2, s1]);
+}
+
+
