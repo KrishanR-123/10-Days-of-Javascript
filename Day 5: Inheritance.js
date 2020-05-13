ed997ee589fe239c47a8a200693664b57e4e4a5e
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-13 16:15:35 +0530

    Hackerrank solution

--- Day 5: Inheritance.js
+++ Day 5: Inheritance.js
@@ -22,3 +22,14 @@ class Rectangle {
             this.w = s;
         }
     };
+
+if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify([ 'constructor' ])) {
+    const rec = new Rectangle(3, 4);
+    const sqr = new Square(3);
+    
+    console.log(rec.area());
+    console.log(sqr.area());
+} else {
+    console.log(-1);
+    console.log(-1);
+}
