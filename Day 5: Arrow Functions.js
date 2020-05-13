4e042973b50a7933df5d1520e39b3b3bcd40421f
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-13 16:26:42 +0530

    Update Day 5: Arrow Functions.js

--- Day 5: Arrow Functions.js
+++ Day 5: Arrow Functions.js
@@ -23,3 +23,21 @@ function readLine() {
 }
 
 
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+
+    function modifyArray(nums) {
+    var something = function(n){
+        if(n%2==0)
+            return n*2;
+        else
+            return n*3;
+    }
+    var newArray = nums.map(something);
+    return newArray;
+    
+}
