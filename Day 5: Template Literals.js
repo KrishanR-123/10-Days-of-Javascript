4296a787acce038844e93e8e210a81fdfcc547e1
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-13 16:24:19 +0530

    Hackerrank solution

--- Day 5: Template Literals.js
+++ Day 5: Template Literals.js
@@ -43,4 +43,14 @@ function readLine() {
     return ([s2, s1]);
 }
 
-
+function main() {
+    let s1 = +(readLine());
+    let s2 = +(readLine());
+    
+    [s1, s2] = [s1, s2].sort();
+    
+    const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
+    
+    console.log((s1 === x) ? s1 : -1);
+    console.log((s2 === y) ? s2 : -1);
+}
