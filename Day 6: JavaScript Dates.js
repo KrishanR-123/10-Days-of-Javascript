4ad49c6d794c2b0495bb969064fc57e8da909853
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-14 16:19:49 +0530

    Update Day 6: JavaScript Dates.js

--- Day 6: JavaScript Dates.js
+++ Day 6: JavaScript Dates.js
@@ -21,3 +21,10 @@ process.stdin.on('end', _ => {
 function readLine() {
     return inputString[currentLine++];
 }
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    const day = new Date(dateString);
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return weekDays[day.getDay()];
+}
