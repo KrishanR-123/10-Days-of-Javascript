0782f8ef38371eb0e02e65b3be9b7ba022d3e882
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-14 16:14:01 +0530

    Update Day 6: Bitwise Operators.js

--- Day 6: Bitwise Operators.js
+++ Day 6: Bitwise Operators.js
@@ -22,4 +22,14 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-
+function getMaxLessThanK(n, k) {
+    let max_anb = 0;
+    for (let b = n; b > 0; b--) {
+        for (let a = b-1; a > 0; a--) {
+            if ((a & b) < k && (a & b) > max_anb){
+                max_anb = (a&b);
+            }
+        }
+    }
+    return max_anb;
+}
