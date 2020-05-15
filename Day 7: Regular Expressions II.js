eee87db15902144bfede9ae4d66d947fd00fb63f
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-15 13:03:00 +0530

    Update Day 7: Regular Expressions II.js

--- Day 7: Regular Expressions II.js
+++ Day 7: Regular Expressions II.js
@@ -21,3 +21,18 @@ process.stdin.on('end', _ => {
 function readLine() {
     return inputString[currentLine++];
 }
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    
+    
+    /*
+     * Do not remove the return statement
+     */
+    let re = new RegExp('^(Mr|Mrs|Ms|Dr|Er)(\\.)([a-zA-Z])*$');
+    return re;
+}
