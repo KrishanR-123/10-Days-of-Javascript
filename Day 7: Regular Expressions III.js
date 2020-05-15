caeba390b54fbec07ebe767b0f7e2c7411c9350e
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-15 13:06:41 +0530

    Update Day 7: Regular Expressions III.js

--- Day 7: Regular Expressions III.js
+++ Day 7: Regular Expressions III.js
@@ -21,3 +21,17 @@ process.stdin.on('end', _ => {
 function readLine() {
     return inputString[currentLine++];
 }
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    
+    
+    /*
+     * Do not remove the return statement
+     */
+     return new RegExp('\\d+', 'g');
+    return re;
+}
