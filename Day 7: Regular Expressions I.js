527aeb76abc255749dee29b649ec91a81a4e1a10
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-15 13:00:48 +0530

    Hackerrank solution

--- Day 7: Regular Expressions I.js
+++ Day 7: Regular Expressions I.js
@@ -35,3 +35,10 @@ function regexVar() {
      let re = /^([aeiou]).+\1$/;
     return re;
 }
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(re.test(s));
+}
