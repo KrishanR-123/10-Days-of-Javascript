847b6b7f04d90d9a488883ff0402e1c5a8041d4f
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-15 13:02:37 +0530

    Update Day 7: Regular Expressions II.js

--- Day 7: Regular Expressions II.js
+++ Day 7: Regular Expressions II.js
@@ -1,2 +1,23 @@
-//just this
-let re = new RegExp('^(Mr|Mrs|Ms|Dr|Er)(\\.)([a-zA-Z])*$');
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
